e0a550682b772e7d0edf2bed1fe9651ad3cfd962
plaintalkjon <davishaysjon@gmail.com>
2024-08-15 11:30:46 -0400

    10 minutes into the video

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # WordleClone
+I'm following a tutorial online that teaches how to program Wodle in JavaScript in 20 minutes.
+
+Tutorial: https://www.youtube.com/watch?v=oKM2nQdQkIU
+
+Objectives:
+1. Complete the tutorial.
+2. Aggressively research each step to ensure I understand it thoroughly.
+3. Understand enough to add my own feature to the game beyond what the tutorial offers.
+    a. As of this time, I'm thinking an abandon button that allows you to try again with a new word but with 1 fewer row.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,80 @@
-alert('hello');
+const state = {
+    grid: Array(6)
+        .fill()
+        .map(() => Array(5).fill('')),
+    currentRow: 0,
+    currentCol: 0,
+};
+
+function updateGrid() {
+    for (let i = 0; i < state.grid.length; i++) {
+        for (let j = 0; j < state.grid[i].length; j++) {
+            const box = document.getElementById(`box${i}${j}`)
+            box.textContent = state.grid[i][j]
+        }
+    }
+}
+
+function drawBox(container, row, col, letter = '') {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.id = `box${row}${col}`;
+    box.textContent = letter;
+    
+    container.appendChild(box);
+    return box;
+}
+
+// Creating the gameboard for Wordle
+function drawGrid(container) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+
+    // Add i number of Rows
+    for (let i = 0; i < 6; i++) {
+        
+        // Add j number of Columns
+        for (let j = 0; j < 5; j++) {
+            drawBox(grid, i, j,);
+        }
+    }
+    container.appendChild(grid);
+}
+
+function registerKeyboardEvents() {
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+        if (key === 'Enter') {
+            if (state.currentCol ===5) {
+                const word = getCurrentWord();
+                if (isWordValid(word)) {
+                    revealWord(word);
+                    state.currentRow++;
+                    state.currentCol = 0;
+                } else {
+                    alert ('Not a valid word.');
+                }
+            }
+        }
+        
+        if (key === 'Backspace') {
+            removeLetter();
+        }
+
+        if (isLetter(key)) {
+            addLetter(key);
+        }
+    }
+
+    updateGrid();
+}
+
+function startup () {
+    const game = document.getElementById('game');
+    drawGrid(game);
+
+    registerKeyboardEvents();
+}
+
+startup();
+
